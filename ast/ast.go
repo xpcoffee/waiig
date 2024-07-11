661322cbbd880f1a67ad09568da5a457b78300b9
Emerick Bosch <xpc.dev@gmail.com>
2024-07-11 09:17:51 +0200

    Add string representation of AST nodes

--- ast/ast.go
+++ ast/ast.go
@@ -1,9 +1,11 @@
 package ast
 
 import "monkey/token"
+import "bytes"
 
 type Node interface {
 	TokenLiteral() string
+	String() string
 }
 
 type Statement interface {
@@ -29,6 +31,16 @@ func (p *Program) TokenLiteral() string {
 	}
 }
 
+func (p *Program) String() string {
+	var out bytes.Buffer
+
+	for _, s := range p.Statements {
+		out.WriteString(s.String())
+	}
+
+	return out.String()
+}
+
 // let
 type LetStatement struct {
 	Token token.Token
@@ -39,14 +51,57 @@ type LetStatement struct {
 func (ls *LetStatement) statementNode()       {}
 func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
 
+func (ls *LetStatement) String() string {
+	var out bytes.Buffer
+	out.WriteString(ls.TokenLiteral())
+	out.WriteString(" ")
+	out.WriteString(ls.Name.String())
+	out.WriteString(" = ")
+	if ls.Value != nil {
+		out.WriteString(ls.Value.String())
+	} else {
+		out.WriteString("nil")
+	}
+	out.WriteString(";")
+
+	return out.String()
+}
+
 // return
 type ReturnStatement struct {
-	Token token.Token
-	Value Expression
+	Token       token.Token
+	ReturnValue Expression
 }
 
 func (rs *ReturnStatement) statementNode()       {}
 func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
+func (rs *ReturnStatement) String() string {
+	var out bytes.Buffer
+	out.WriteString(rs.TokenLiteral())
+	out.WriteString(" ")
+	if rs.ReturnValue != nil {
+		out.WriteString(rs.ReturnValue.String())
+	} else {
+		out.WriteString("nil")
+	}
+	out.WriteString(";")
+	return out.String()
+}
+
+// expression statement
+type ExpressionStatement struct {
+	Token token.Token // the first token in the expression
+	Value Expression
+}
+
+func (es *ExpressionStatement) statementNode()       {}
+func (es *ExpressionStatement) TokenLiteral() string { return es.Token.Literal }
+func (es *ExpressionStatement) String() string {
+	if es.Value != nil {
+		return es.Value.String()
+	}
+	return "nil"
+}
 
 // identifier
 type Identifier struct {
@@ -56,3 +111,7 @@ type Identifier struct {
 
 func (i *Identifier) expressionNode()      {}
 func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
+
+func (i *Identifier) String() string {
+	return i.Value
+}
