90186e90ce007314e1557123914c41cfa87f9528
Emerick Bosch <xpc.dev@gmail.com>
2024-07-30 22:07:53 +0200

    Update main function to run grapher

--- main.go
+++ main.go
@@ -2,18 +2,33 @@ package main
 
 import (
 	"fmt"
+	"monkey/grapher"
 	"monkey/repl"
 	"os"
 	"os/user"
 )
 
 func main() {
+	runRepl()
+}
+
+func runRepl() {
 	user, err := user.Current()
 	if err != nil {
 		panic(err)
 	}
-
-	fmt.Printf("Hello, %s! This is the Monkey programming language!\n", user.Username)
-	fmt.Printf("Feel free to type commands\n")
+	fmt.Printf("Hello, %s! Welcome to the Monkey programming language!\n", user.Username)
 	repl.Start(os.Stdin, os.Stdout)
+}
+
+func graphAst() {
+	input := `
+	   let hello = fn(x,y) {
+	       fn(z) {
+	           x + y + z;
+	       }
+	   }; hello(1,2)(3);
+	   `
+	graph := grapher.New(input).GetDot()
+	fmt.Println(graph)
 }
